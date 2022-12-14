a91af03a7cbdda162f763ff153a18cb1971696a6
Johannes C. Mayer <j.c.mayer240@gmail.com>
2022-12-14 02:19:45 +0000

    add size functionality:w

--- draw-image.el
+++ draw-image.el
@@ -4,6 +4,18 @@
                               (buffer-substring (line-beginning-position) (line-end-position))))
 
 ;;;###autoload
+(defun set-img-size ()
+  "Insert latex and html header for image size."
+  (interactive)
+  (cond
+   ((equal current-prefix-arg nil)
+    (setq n 500))
+   (t
+    (setq n (read-number "Image width in px: "))))
+  (insert (format "#+attr_latex: :width %spx\n" n))
+  (insert (format "#+attr_html:  :width %spx\n" n)))
+
+;;;###autoload
 (defun draw-image ()
   (interactive)
   (setq file-path (get-link))
@@ -11,6 +23,7 @@
     (shell-command-to-string
      (concat "draw-image " (file-name-base file-path))))
   (delete-region (line-beginning-position) (line-end-position))
+  (set-img-size)
   (insert output-link)
   (org-display-inline-images)
   (org-redisplay-inline-images))
